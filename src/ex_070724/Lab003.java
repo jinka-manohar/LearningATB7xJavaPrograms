86c50fb1428e5f9b4c1fc502d966c56938cf11c3
jinka-manohar <jmm16020ee025@gmail.com>
2024-07-11 16:15:33 +0530

    All Files

--- src/ex_070724/Lab003.java
+++ src/ex_070724/Lab003.java
@@ -1,7 +1,7 @@
 package ex_070724;
 
 public class Lab003 {
-    public static void main(String[] jinka) {       //This args can be named with anything
+    public static void main(String[] jinka) {       //This args can be named with anything(jinka)
         System.out.println("Exercise just");
     }
 }
